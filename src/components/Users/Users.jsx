a102d132827e4e1bada5c7522881f410fd83647f
pinokio221 <pinokio221b@gmail.com>
2021-02-09 15:40:06 +0200

    Added Users API

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -8,55 +8,13 @@ import * as axios from "axios";
 
 const Users = (props) => {
 
-    let getUsers = () => {
-        if(props.usersPage.usersList.length === 0) {
-            axios.get("https://social-network.samuraijs.com/api/1.0/users")
-                .then(response => {
-                    props.setUsers([
-                        {
-                            id: 1,
-                            fullname: "Alex Menco",
-                            age: 29,
-                            city: "Los Angeles",
-                            isFriend: false,
-                            friendInventation: false,
-                            profileImage: "https://d3b4yo2b5lbfy.cloudfront.net/wp-content/uploads/2019/06/d6a1f2019-CP-Forum-Avatars-TealfulEyes-Kodan.png"
-                        },
-                        {
-                            id: 2,
-                            fullname: "Boris Gulyav",
-                            age: 31,
-                            city: "Moscow",
-                            isFriend: false,
-                            friendInventation: true,
-                            profileImage: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQecV_20snjsbgZSClHCml7tnMWvSYCD7ojqQ&usqp=CAU"
+    let requestURL = "http://localhost:9000/users";
 
-                        },
-                        {
-                            id: 3,
-                            fullname: "Genadiy Bukin",
-                            age: 42,
-                            city: "Ekaterinburg",
-                            isFriend: false,
-                            friendInventation: false,
-                            profileImage: "https://i2.wp.com/avatarfiles.alphacoders.com/161/161678.jpg"
-
-                        },
-                        {
-                            id: 4,
-                            fullname: "Yuriy Proskyrok",
-                            age: 54,
-                            city: "Ekaterinburg",
-                            isFriend: true,
-                            friendInventation: false,
-                            profileImage: "https://avatarfiles.alphacoders.com/181/181460.jpg"
-
-                        }]
-                    )
-                })
-
-        }
-
+    if(props.usersPage.usersList.length === 0) {
+        axios.get(requestURL)
+            .then(response => {
+                props.setUsers(response.data.users)
+            })
     }
 
     let newSearchElement = React.createRef();
